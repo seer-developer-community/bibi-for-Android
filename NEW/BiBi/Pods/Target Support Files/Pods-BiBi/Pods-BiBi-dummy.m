5a71f1a204b7b08d95cf032157a7321c18b36478
武建斌 <602168099@qq.com>
2019-12-23 18:24:15 +0800

    no message

--- NEW/BiBi/Pods/Target Support Files/Pods-BiBi/Pods-BiBi-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BiBi : NSObject
-@end
-@implementation PodsDummy_Pods_BiBi
-@end
--- NEW/BiBi/ThridTools/BitsharesWallet/Operations/OperationsHeader.h
+++
@@ -1,9 +0,0 @@
-//
-//  OperationsHeader.h
-//  BitsharesWallet
-//
-//  Created by flh on 2018/8/30.
-//  Copyright © 2018年 flh. All rights reserved.
-//
-
-#import "TransferOperation.h"
